919bdc0f0c89b119239a089272780ee726ec3772
sampathvenur <sampathkumarvenur@gmail.com>
2025-04-05 15:58:01 +0530

    components api - button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import { Typography, Button, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
 
 const App = () => {
     return (
@@ -25,13 +25,27 @@ const App = () => {
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible so we can see how does it look like on the screen.
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} justifyContent="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
-                <ButtonGroup variant="contained" aria-label="Basic button group">
+                {/* <ButtonGroup variant="contained" aria-label="Basic button group">
                     <Button>One</Button>
                     <Button>Two</Button>
                     <Button>Three</Button>
-                </ButtonGroup>
+                </ButtonGroup> */}
             </main>
         </>
     );
