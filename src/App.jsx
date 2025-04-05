fb6c8b5b73c0e1e2c94fcb930e508163721756d0
sampathvenur <sampathkumarvenur@gmail.com>
2025-04-05 15:28:33 +0530

    react and material ui setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+const  App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello World!!</Typography>
+        </div>
+    );
+}
+
+export default App;
