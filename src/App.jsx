d1b66e43a41584ab5cdbb7eba64170cf166a250e
sampathvenur <sampathkumarvenur@gmail.com>
2025-04-05 15:45:16 +0530

    material ui templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 const  App = () => {
     return (
-        <div>
-            <Typography variant="h1">Hello World!!</Typography>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible so we can see how does it look like on the screen.
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
     );
 }
 
